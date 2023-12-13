81a36a61b418e106ce7084fb88e18d3df32e5fc0
Vishal_SR <52326540+Vishal-SR-505@users.noreply.github.com>
2023-12-13 22:10:54 +0530

    Update polyfills.js

--- javascript/polyfills.js
+++ javascript/polyfills.js
@@ -20,3 +20,29 @@ let output = arr.myMap((ele,index,arr)=>{
 })
 
 console.log("output :: ",output);
+
+
+
+// filter() polyfills
+
+const arr = [1,2,3,4,5];
+
+Array.prototype.myfilter = function (cb){
+
+let temp = [];
+
+    for (let i=0;i<this.length;i++){
+if(cb(this[i],i,this)){
+    temp.push(this[i]);
+}
+    }
+    return temp;
+}
+
+
+let output = arr.myfilter((ele,index,arr)=>{
+
+    return ele>=4;
+})
+
+console.log("output :: ",output);
